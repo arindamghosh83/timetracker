caa9e5e9396478e588e137043c320b9e533bdabe
Gueye, Pathe, F <pfgueye@CMH.edu>
2018-08-30 14:01:59 -0500

    Project setup

---
+++ Timetracker.Functions/Inject/InjectAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Azure.WebJobs.Description;
+
+namespace Timetracker.Functions.Inject
+{
+	//https://blog.wille-zone.de/post/azure-functions-dependency-injection/
+
+	[Binding]
+	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+	public class InjectAttribute : Attribute
+	{
+		public InjectAttribute(Type type)
+		{
+			Type = type;
+		}
+		public Type Type { get; }
+	}
+}
---
+++ Timetracker.Functions/Inject/InjectConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure.WebJobs.Host.Config;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Timetracker.Functions.Inject
+{
+	public class InjectConfiguration : IExtensionConfigProvider
+	{
+		private IServiceProvider _serviceProvider;
+		public void Initialize(ExtensionConfigContext context)
+		{
+			
+
+			var services = new ServiceCollection();
+			RegisterServices(services);
+			_serviceProvider = services.BuildServiceProvider(true);
+
+			context
+				.AddBindingRule<InjectAttribute>()
+				.BindToInput<dynamic>(i => _serviceProvider.GetRequiredService(i.Type));
+		}
+
+		private static void RegisterServices(IServiceCollection services)
+		{
+			
+		}
+	}
+}
